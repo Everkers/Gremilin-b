7d5facce7834dccc63c49858eff94a4e1c23a2b8
everkers <waelkourich12@gmail.com>
2019-12-28 13:36:51 +0100

    delete useless files

--- utils/ban_suggestion.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-async function banSugg(champ){
-    const url = `http://arrax-lol.herokuapp.com/api/ban-suggestion/${champ}`
-    const res = await axios.get(url)
-    const list = res.data;
-    return list
-}
-module.exports = banSugg
--- utils/build.js
+++
@@ -1,7 +0,0 @@
-const axios =require('axios');
-async function build(champ){
-    const res = await axios.get(`http://arrax-lol.herokuapp.com/api/items/${champ}`)
-    const data = res.data;
-    return data
-}
-module.exports = build
