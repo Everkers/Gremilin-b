3bcfd6c5dc276661560df8fb16a67c4d8b7106b8
everkers <waelkourich12@gmail.com>
2020-01-24 17:56:25 +0100

    add cooldown

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const discord_token = process.env.TOKEN_BOT
+const discord_token = process.env.TOKEN_DEVBOT
 const Profile = require('./utils/profile')
 const ImageEditor = require('./utils/imageEditor')
 const Champion = require('./utils/championData')
@@ -9,6 +9,7 @@ const Points = require('./utils/pointsChanges')
 const imageEditor = new ImageEditor()
 const profile = new Profile()
 const champion = new Champion()
+const cooldown = new Set()
 const commands = {
 	profile: { regex: new RegExp('profile', 'gis'), execute: profile.getData },
 	setSummoner: {
@@ -69,103 +70,111 @@ client.on('ready', () => {
 client.on('message', async message => {
 	const content_msg = message.content
 	if (content_msg.startsWith('?')) {
-		if (content_msg.match(commands.profile.regex)) {
-			commands.profile.execute(message, client)
-		} else if (content_msg.match(commands.setSummoner.regex)) {
-			commands.setSummoner.execute(message)
-		} else if (content_msg.match(commands.updateSummoner.regex)) {
-			commands.updateSummoner.execute(message)
-		} else if (content_msg.match(commands.imageEditor.regex)) {
-			commands.imageEditor.execute(message)
-		} else if (content_msg.match(commands.champion.regex)) {
-			commands.champion.execute(message)
-		} else if (content_msg.match(commands.borders.regex)) {
-			const messageStyles1 = new Discord.RichEmbed()
-				.setTitle('Gremilin Borders')
-				.addField(
-					'Border number 1',
-					'You can start using this border once you hit 50gp'
-				)
-				.setImage(
-					'https://media.discordapp.net/attachments/657198405852069893/670017837300973568/761.png?width=289&height=430'
-				)
-			const messageStyles2 = new Discord.RichEmbed()
-				.setTitle('Gremilin Borders')
-				.addField(
-					'Border number 2',
-					'You can start using this border once you hit 200gp'
-				)
-				.setImage(
-					'https://media.discordapp.net/attachments/668898486523133975/670018086363201556/527.png?width=289&height=430'
-				)
-			message.channel.send(messageStyles1)
-			message.channel.send(messageStyles2)
-			message.channel.send('``?points to see how much points you have``')
-		} else if (content_msg.match(commands.points.regex)) {
-			const points = new Points(message.author.id)
-			const pts = await points.getPoints
-			if (pts !== false) {
-				message.channel.send(`You have ${pts}gp`)
-			} else {
-				message.channel.send(
-					'Something went wrong, Please set the user first , Use ``?setUser [summoner name] [summoner region]`` to set the user.'
-				)
-			}
-		} else if (content_msg.match(commands.help.regex)) {
-			const messageStyles = new Discord.RichEmbed()
-				.setTitle('Gremilin Commands')
-				.setDescription('Gp = Gremilin Points')
-				.addField(
-					'**Important**',
-					'If your username on league of legends contains more than one word then wrap the name with the double quotation marks \n ``example: ?updateUser "FNC MagiFelix" euw``'
-				)
-				.addBlankField()
-				.addField(
-					'`` ?setUser [username] [region] ``',
-					'This command will add your league of legends account to Grimilin mind, next time he will remember you :)',
-					true
-				)
-				.addField(
-					'`` ?updateUser [username] [region]``',
-					'This command will update your league of legends account if already registered.',
-					true
-				)
-				.addField(
-					'`` ?profile ``',
-					'This command will give you some informations about your account like last match etc...',
-					true
-				)
-				.addField(
-					'`` ?champion [champion name] ``',
-					'This command will give you all that you need to know about a specific chmapion.',
-					true
-				)
-				.addField(
-					'`` ?editMe [border number]``',
-					'This command will edit your discord profile picture.',
-					true
-				)
-				.addField(
-					'``?points``',
-					'This command will show you how much gp you have.',
-					true
-				)
-				.addField(
-					'``?borders``',
-					'This command will show you all the available borders.',
-					true
-				)
+		if (cooldown.has(message.author.id)) {
+			message.reply('cannot use that command just yet! wait for 30 seconds.')
+		} else {
+			cooldown.add(message.author.id)
+			setTimeout(() => {
+				cooldown.delete(message.author.id)
+			}, 30000)
+			if (content_msg.match(commands.profile.regex)) {
+				commands.profile.execute(message, client)
+			} else if (content_msg.match(commands.setSummoner.regex)) {
+				commands.setSummoner.execute(message)
+			} else if (content_msg.match(commands.updateSummoner.regex)) {
+				commands.updateSummoner.execute(message)
+			} else if (content_msg.match(commands.imageEditor.regex)) {
+				commands.imageEditor.execute(message)
+			} else if (content_msg.match(commands.champion.regex)) {
+				commands.champion.execute(message)
+			} else if (content_msg.match(commands.borders.regex)) {
+				const messageStyles1 = new Discord.RichEmbed()
+					.setTitle('Gremilin Borders')
+					.addField(
+						'Border number 1',
+						'You can start using this border once you hit 50gp'
+					)
+					.setImage(
+						'https://media.discordapp.net/attachments/657198405852069893/670017837300973568/761.png?width=289&height=430'
+					)
+				const messageStyles2 = new Discord.RichEmbed()
+					.setTitle('Gremilin Borders')
+					.addField(
+						'Border number 2',
+						'You can start using this border once you hit 200gp'
+					)
+					.setImage(
+						'https://media.discordapp.net/attachments/668898486523133975/670018086363201556/527.png?width=289&height=430'
+					)
+				message.channel.send(messageStyles1)
+				message.channel.send(messageStyles2)
+				message.channel.send('``?points to see how much points you have``')
+			} else if (content_msg.match(commands.points.regex)) {
+				const points = new Points(message.author.id)
+				const pts = await points.getPoints
+				if (pts !== false) {
+					message.channel.send(`You have ${pts}gp`)
+				} else {
+					message.channel.send(
+						'Something went wrong, Please set the user first , Use ``?setUser [summoner name] [summoner region]`` to set the user.'
+					)
+				}
+			} else if (content_msg.match(commands.help.regex)) {
+				const messageStyles = new Discord.RichEmbed()
+					.setTitle('Gremilin Commands')
+					.setDescription('Gp = Gremilin Points')
+					.addField(
+						'**Important**',
+						'If your username on league of legends contains more than one word then wrap the name with the double quotation marks \n ``example: ?updateUser "FNC MagiFelix" euw``'
+					)
+					.addBlankField()
+					.addField(
+						'`` ?setUser [username] [region] ``',
+						'This command will add your league of legends account to Grimilin mind, next time he will remember you :)',
+						true
+					)
+					.addField(
+						'`` ?updateUser [username] [region]``',
+						'This command will update your league of legends account if already registered.',
+						true
+					)
+					.addField(
+						'`` ?profile ``',
+						'This command will give you some informations about your account like last match etc...',
+						true
+					)
+					.addField(
+						'`` ?champion [champion name] ``',
+						'This command will give you all that you need to know about a specific chmapion.',
+						true
+					)
+					.addField(
+						'`` ?editMe [border number]``',
+						'This command will edit your discord profile picture.',
+						true
+					)
+					.addField(
+						'``?points``',
+						'This command will show you how much gp you have.',
+						true
+					)
+					.addField(
+						'``?borders``',
+						'This command will show you all the available borders.',
+						true
+					)
 
-				.addField(
-					'``How can i earn gp?``',
-					'You can start earning gp by using the bot, on each command you use, you earn certain amount of points',
-					true
-				)
-				.setFooter(
-					`Developed with love by Everkers#6416`,
-					'https://i.pinimg.com/236x/f0/10/b2/f010b2798bfaa02c4afd72cb2aef6bfc.jpg'
-				)
-			message.channel.send(messageStyles)
+					.addField(
+						'``How can i earn gp?``',
+						'You can start earning gp by using the bot, on each command you use, you earn certain amount of points',
+						true
+					)
+					.setFooter(
+						`Developed with love by Everkers#6416`,
+						'https://i.pinimg.com/236x/f0/10/b2/f010b2798bfaa02c4afd72cb2aef6bfc.jpg'
+					)
+				message.channel.send(messageStyles)
+			}
 		}
 	}
 })
